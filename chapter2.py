e890664afd844c54058756c36f6b0b4d149b0568
jltichy <jltichy@gmail.com>
2016-08-25 16:18:30 +0000

    examples through functions

--- chapter2.py
+++ chapter2.py
@@ -34,8 +34,32 @@ print match       #"<function re.match>"
 # print '5 / 2' #This isn't working, and I'm not sure why.
 
 def double(x):
-  """this is where you put an optional docstring that explains what the function does.  for example, this function multiplies its input by 2"""
+  """this is where you put an optional : that explains what the 
+  function does.  for example, this function multiplies its input by 2"""
   return x*2
   
 x = 5
 print double(x)
+
+def apply_to_one(f):
+  """calls the function f with 1 as its argument"""
+  return f(1)
+
+my_double = double    #refers to the previously defined function
+x = apply_to_one(my_double) #equals 2
+y = apply_to_one(lambda x: x + 4)   #equals 5
+#another_double = lambda x: 2 * x    #don't do this
+def another_double(x): return 2 * x #do this instead
+
+def my_print(message="my default message"):
+  print message
+
+my_print("hello")     #prints 'hello'
+my_print()            #prints 'my default message'
+
+def subtract(a=0, b=0):
+  return a - b
+subtract(10,5) #returns 5
+subtract(0,5)   #returns -5
+subtract(b=5)   #also returns -5
+
