5133e161bd915c321a761b704a328cbc8ee5f834
jltichy <jltichy@gmail.com>
2016-09-20 20:05:10 +0000

    getting stuck with bug

--- chapter2.py
+++ chapter2.py
@@ -358,3 +358,42 @@ print all([
 #by convention, we give classes PascalCase names
 class Set:
   
+  #these are the member functions
+  #every one takes a first parameter "self" (another convention)
+  #that refers to the particular Set object being used
+  
+  def __init__(self, values=None):
+  # This is the constructor.
+  # It gets called when you create a new Set.
+  # You would use it like this:
+  # s1 = Set()          #empty set
+  # s2 = Set([1,2,2,3]) #it initialize with values
+
+    self.dict = {}  #each instance of Set has its own dict property
+  #which is what we'll use to track memberships
+    if values is not None:
+      for value in values:
+        self.add(value)
+        
+  def __repr__(self):
+    #this is the string representation of a Set object
+    #if you type it at the Python prompt or pass it to str()
+    return "Set: " + str(self.dict.keys())
+    
+  #we'll represent membership by being a key in self.dict with value True
+  def add(self,value):
+    return value in self.dict
+    
+  def remove(self,value):
+    del self.dict[value]
+    
+#which we could then use like this:
+s = Set([1,2,3])
+s.add(4)
+print s.contains(4)   #true
+s.remove(3)   
+print s.contains(3)   #true
+
+#Functional Tools - page 31
+
+      
